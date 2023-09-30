74942d981c25bc86e23530e76253a87dc1f6239a
Shreyas <90107985+NotShrey@users.noreply.github.com>
2023-09-30 13:32:25 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (93.01%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -3,8 +3,9 @@ class Solution {
         int low = 0,high = arr.length-1;
         while(low<=high){
             int mid = (low+high)/2;
-            int missing = arr[mid]-(mid+1);
-            if(missing<k){
+            int missing = arr[mid]-(mid+1); // 7 is mid - 7's index + 1 which is 3+1 = 4 
+                                            // now missing will have the val as 7-4 = 3
+            if(missing<k){                  // 3 is less than 5 is true so move right
                 low = mid+1;
             }
             else{
@@ -15,3 +16,9 @@ class Solution {
         
     }
 }
+
+// 0 1 2 3 4  array index
+//[2,3,4,7,11]
+// 1 2 3 4 5   default way
+
+
