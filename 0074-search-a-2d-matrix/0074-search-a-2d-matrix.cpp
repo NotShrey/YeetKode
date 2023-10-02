a79e6ce5b0314e434cf907b8a1e19a5230b6e217
Shreyas <90107985+NotShrey@users.noreply.github.com>
2023-10-02 23:07:06 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (6.52%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,15 +1,62 @@
 class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i  = 0; i<matrix.size(); ++i)
-        {
-            for(int j = 0; j<matrix[0].size(); ++j)
-            {
-                if(matrix[i][j] == target)
-                    return true;
+    
+private:
+    int bs(vector<int>& row, int target){
+        int low = 0, high = row.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (row[mid] == target) {
+                return mid;
+            } else if (row[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
+        
+        return -1; // Return -1 to indicate that the target was not found
+    }
+    
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return false;
+        }
+        
+        for (int i = 0; i < matrix.size(); ++i) {
+            if (matrix[i][0] <= target && target <= matrix[i][matrix[i].size() - 1]) {
+                int result = bs(matrix[i], target);
+                if (result != -1) {
+                    return true;
+                }
+            }
+        }
+        
         return false;
     }
-	
 };
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         for(int i  = 0; i<matrix.size(); ++i)
+//         {
+//             for(int j = 0; j<matrix[0].size(); ++j)
+//             {
+//                 if(matrix[i][j] == target)
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }
+	
+// };
