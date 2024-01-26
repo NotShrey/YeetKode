a8e9073d78576c3ca9bea9f9c5d26082f99e89b7
Shreyas <90107985+NotShrey@users.noreply.github.com>
2024-01-26 15:36:55 +0530

    Time: 12 ms (39.85%), Space: 18.5 MB (5.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,84 +1,17 @@
-//better approach tc-> o(n) but sc -> log(n) because of odered map
-
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ls;
-        map<int,int> mpp;
-        
-        int mini  = (n/3)+1;
-        
-        for(int i = 0;i<n;i++){
-           int added = ++mpp[nums[i]];
-            
-            if( added == mini){
-                ls.push_back(nums[i]);
+        vector<int> ans;
+        map<int,int>mpp;
+        int mini = ((nums.size()/3)+1);
+        for(int i=0; i<nums.size(); i++){
+            mpp[nums[i]]++;
+            if(mpp[nums[i]] == mini){
+                ans.push_back(nums[i]);
             }
-            if(ls.size()==2){
-                break;
-            }
+            if(ans.size() == 2) break;
         }
-        sort(ls.begin(),ls.end());
-        return ls;
+        return ans;
+        
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // brute force approach tc-> o(n^2)
-// class Solution {
-// public:
-//     vector<int> majorityElement(vector<int>& nums) {
-//         int n = nums.size();
-//         vector<int> ls;
-
-//         for (int i = 0; i < n; i++) {
-//             if (ls.size() == 0 || ls[0] != nums[i]) {
-//                 int cnt = 0;
-//                 for (int j = 0; j < n; j++) {
-//                     if (nums[i] == nums[j]) {
-//                         cnt++;
-//                     }
-
-//                     if (cnt > n / 3) {
-//                         ls.push_back(nums[i]);
-//                         break;
-//                     }
-//                 }
-//             }
-
-//             if (ls.size() == 2) {
-//                 break;
-//             }
-//         }
-
-//         return ls;
-//     }
-// };
