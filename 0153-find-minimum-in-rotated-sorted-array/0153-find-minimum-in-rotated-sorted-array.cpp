367a136cd6783334d1d2f30ed00d2018720f5ea4
Shreyas <90107985+NotShrey@users.noreply.github.com>
2024-01-30 15:45:36 +0530

    Time: 7 ms (13.93%), Space: 12.6 MB (10.83%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,43 +1,22 @@
-// tc (log n) sc(1)
-
-
 class Solution {
 public:
     int findMin(vector<int>& nums) {
         int n = nums.size();
-        int l = 0;
-        int h = n - 1;
+        int l = 0, h = n - 1;
+        if(l>h) return 0;
         int ans = INT_MAX;
 
         while (l <= h) {
-            int mid = (h+l) / 2;// this l+h-l/2 didnt work here id why
+            int mid = (h + l) / 2;
+
             if (nums[l] <= nums[mid]) {
-                ans = min(ans, nums[l]);
+                ans = min(ans,nums[l]);
                 l = mid + 1;
             } else {
                 h = mid - 1;
-                ans = min(ans, nums[mid]);
-                
+                ans = min(ans,nums[mid]);
             }
         }
         return ans;
     }
 };
-
-
-
-// class Solution {
-// public:
-//     int findMin(vector<int>& nums) {
-//         int sm = INT_MAX;
-
-//         for(int i = 0;i<nums.size();i++){
-//             if(nums[i]<sm){
-//                 sm = nums[i];
-//             }
-//         }
-//         return sm;
-        
-//     }
-// };
-// //o(n) could be better kal karunga bs laga ke
